fd3325f3a44ec1967cb06e0df3ea012db11a265a
Arvindkumar Dangar <Arvindkumar.Dangar@ibm.com>
2022-02-07 15:41:15 +0530

    es6 support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-function add(num1, num2) {
+export function add(num1, num2) {
     console.log('this is sum function');
     return num1 + num2;
   }
   
-  function subtract(num1, num2) {
+  export function subtract(num1, num2) {
     console.log('this is subtract function');
     return num1 - num2;
   }
-  
-  exports.add = add;
-  exports.subtract = subtract;
