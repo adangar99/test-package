54dc19fa92fdcc25c0db458c480b1fe8c1f00457
Arvindkumar Dangar <Arvindkumar.Dangar@ibm.com>
2022-02-07 16:01:30 +0530

    update to arrow function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-export function add(num1, num2) {
-    console.log('this is sum function');
-    return num1 + num2;
-  }
+export const add = (num1, num2) => num1 + num2;
   
-  export function subtract(num1, num2) {
-    console.log('this is subtract function');
-    return num1 - num2;
-  }
+export const subtract = (num1, num2) => num1 - num2;
+  
